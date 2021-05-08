0ec2d4888637833e3169352b9e4050ab631353e5
Talita Barbosa <talita.bab@gmail.com>
2021-05-08 15:12:52 -0300

    Cria request com Hooks - DataTable Integration

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,6 +1,15 @@
-//função para arredondar
 
+import {format}  from 'date-fns'
+
+//função para arredondar
 export const round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
+}
+
+//formatação de data
+export const formatLocalDate = (date: string, pattern: string) => {
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, pattern);
 }
