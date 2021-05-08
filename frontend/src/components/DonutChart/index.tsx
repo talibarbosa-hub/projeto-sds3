9a0632a3c31364eb8a46089387c96f870a6edb7a
Talita Barbosa <talita.bab@gmail.com>
2021-05-08 11:41:16 -0300

    Primeiro request sem Hooks

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,37 @@
+import axios from 'axios';
+import { type } from 'node:os';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
-    const mockData = {
+
+    //  sem Hooks requisição e tratamento de dados
+    let chartData : ChartData = {labels:[], series:[]}; // inicia com lista vazia
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response=> {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map (x => x.sum );
+
+            chartData = {labels: myLabels, series: mySeries};
+            console.log(chartData);
+        });
+
+
+
+    
+    //mock
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
     
     const options = {
         legend: {
@@ -13,8 +40,8 @@ const DonutChart = () => {
     };
         return (
             <Chart
-                options={{...options, labels: mockData.labels}}  // pega o objeto options e inclui
-                series={mockData.series}
+                options={{...options, labels: chartData.labels}}  // pega o objeto options e inclui
+                series={chartData.series}
                 type="donut"
                 height="240"
 
