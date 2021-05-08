9617edd0a8f33cd90c82177f57fced311f4f4498
Talita Barbosa <talita.bab@gmail.com>
2021-05-08 10:44:33 -0300

    Cria rotas

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {      //Expressão Lambda, atribui a função anonima a uma váriavel
+    return (
+        <div>
+            <h1>Pagina Home</h1>
+        </div>
+    )
+}
+
+export default Home;
